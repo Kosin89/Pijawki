06d76591652cbe9ce9a60aaadee87b1f017e41d8
Kosin89 <110737422+Kosin89@users.noreply.github.com>
2023-01-16 12:15:02 +0100

    Pijawki

---
+++ js/main.js
@@ -0,0 +1,16 @@
+const nav = document.querySelector(".navbar-collapse")
+
+document.addEventListener("click", () => {
+    if (nav.classList.contains("show")) {
+        nav.classList.remove("show")
+    }
+})
+
+document.addEventListener("click",function (e){
+    if(e.target.classList.contains("gallery-item")){
+          const src = e.target.getAttribute("src");
+          document.querySelector(".modal-img").src = src;
+          const myModal = new bootstrap.Modal(document.getElementById('gallery-modal'));
+          myModal.show();
+    }
+  })
